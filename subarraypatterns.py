e73d7306e667d088e859ce665047c4d2672ffeac
Rehen Manoy <rehenmanoy@gmail.com>
2025-11-27 16:18:48 +0530

    Implement subarray sum functions and Kadane's Algorithm
    Added functions to count subarrays with a given sum and to find the maximum subarray sum using Kadane's Algorithm.

--- subarraypatterns.py
+++ subarraypatterns.py
@@ -17,3 +17,45 @@ def subarraySumExists(arr, K):
 arr = [1, 2, 3, 7, 5]
 K = 13
 print(subarraySumExists(arr, K))
+
+#Count Subarrays With Sum = K
+def countSubarraysWithSum(arr, K):
+    count = 0
+    n = len(arr)
+    for i in range(n):
+        current_sum = 0
+        for j in range(i,n):
+            current_sum+=arr[j]
+            if current_sum == K:
+                count += 1
+    return count
+
+arr = [1, 2, 1, 2, 1]
+K = 3
+print(countSubarraysWithSum(arr,K))
+
+#Maximum Subarray Sum (Kadane’s Algorithm) 
+# ✔ Step 1: Initialize
+# current_sum = 0
+# max_sum = -∞ (to handle cases where all numbers are negative)
+# ✔ Step 2: Loop through each element x
+# Add the number to current_sum
+# Update max_sum if current_sum is now the largest
+# If current_sum becomes negative, reset it to 0
+# (because a negative start only hurts future sums)
+# ✔ Step 3: Return max_sum
+def maxSubarraySum(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+
+    for x in arr:
+        current_sum += x
+        if current_sum > max_sum:
+            max_sum = current_sum
+        if current_sum < 0:
+            current_sum = 0
+    
+    return max_sum
+
+arr = [-2, 3, -1, 2, -3]
+print(maxSubarraySum(arr))
